2c784b6040b95441f3b22f6c404433d80cdcf931
Daniel Temple <dantdj@gmail.com>
2021-03-13 18:49:49 +0000

    Add busywork

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"math"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
+		workDone := doWork()
 		c.JSON(200, gin.H{
 			"message": "pong",
+			"work":    workDone,
 		})
 	})
 	r.GET("/pong", func(c *gin.Context) {
+		workDone := doWork()
 		c.JSON(200, gin.H{
 			"message": "ping",
+			"work":    workDone,
 		})
 	})
 	r.Run("0.0.0.0:3000")
+}
+
+func doWork() string {
+	for i := 0; i < 1000000000; i++ {
+		a := 2 * i
+		value := float64(a * i)
+		math.Sqrt(value)
+	}
+	return "done"
 }
